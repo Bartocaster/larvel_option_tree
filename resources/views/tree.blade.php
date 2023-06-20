1f4d2f4b4853596aa346ee218165e800e3973fbc
Bart Huiskamp <bartolameus.h@gmail.com>
2023-06-20 14:02:17 +0200

    all 4 selected box get output

--- resources/views/tree.blade.php
+++ resources/views/tree.blade.php
@@ -1,4 +1,6 @@
 <?php 
+// created the data structure to hold the tree.
+// representing each lower part as next selection
  $array = [
     'A' => [
         '2' => '2',
@@ -6,20 +8,20 @@
         '6' => '6'
         ],
     'B' => [
-            '1' => 'Option 1',
-            '3' => 'Option 3',
-            '10' => [
-                '70' => 'Option 70',
-                'PI' => [
-                    'Test' => 'Option Test',
-                    'Foo' => 'Option Foo',
-                    'Bar' => 'Option Bar'
-                ],
-                'Segma' => 'Option Segma'
-            ]
-        ],
+        '1' => 'Option 1',
+        '3' => 'Option 3',
+        '10' => [
+            '70' => 'Option 70',
+            'PI' => [
+                'Test' => 'Option Test',
+                'Foo' => 'Option Foo',
+                'Bar' => 'Option Bar'
+            ],
+            'Segma' => 'Option Segma'
+        ]
+    ],
     "C" => [
-        '50' => 50,
+        '50' => 'Option 50',
         '60' => [
             'Alpha' => 'Option Alpha',
             '3'=> 'option 3',
@@ -29,36 +31,85 @@
 
             ]
         ];
-function tree(){
-    
-    $select ='<label for="color">Tree Option:</label>
-    <select name="color" id="color">';
 
-    $select .='
-        <option value="">--- Choose a color ---</option>
-        <option value="red"> A </option>
-        <option value="green"> B </option>
-        <option value="blue"> C </option>
-    </select>';
-    return $select;
-}
+// // first rough idea.
+// function tree(){
+    
+//     $select ='<label for="color">Tree Option:</label>
+//     <select name="color" id="color">';
+
+//     $select .='
+//         <option value="">--- Choose a color ---</option>
+//         <option value="red"> A </option>
+//         <option value="green"> B </option>
+//         <option value="blue"> C </option>
+//     </select>';
+//     return $select;
+// }
 function generateSelectBoxes($array, $level = 0) {
     $select = '<select class="select-box" data-level='.$level.'>
             <option value="">Select Option</option>';
         // possilbe if statment could bring me far.
+        $select2 = '<select class="select-box" data-level=1>
+            <option value="">Select Option</option>';
+            $select3 = '<select class="select-box" data-level=2>
+            <option value="">Select Option</option>';
+                $select4 = '<select class="select-box" data-level=2>
+                <option value="">Select Option</option>';
     foreach ($array as $key => $value) {
-        echo '<pre>'; var_dump($value); echo '</pre>';
+        // echo '<pre>'; var_dump($value); echo '</pre>';
         // print_r($value);
         // print_r($key);
+        // trouble understanding wy it can't be formed in 1 line.
+        // $select .= '<option value="' . $key . '" ' . $select . '>' . $key . '</option>';
         $select .= '<option value='.$key.'>';
         $select .= $key;
         $select .='</option>';
-       
+        // print_r($key);
+    
+        foreach ($array[$key] as $lvl1 => $lvl1_val) {
+            // echo '<pre>'; var_dump($array[$key]); echo '</pre>';
+            $select2 .= '<option value='.$lvl1.'>';
+            $select2 .= $lvl1;
+            $select2 .='</option>';
+
+
+            if(is_array($array[$key][$lvl1])){
+                foreach ($array[$key][$lvl1] as $lvl2 => $lvl2_val) {
+                    // echo '<pre>'; var_dump($array[$key]); echo '</pre>';
+                    $select3 .= '<option value='.$lvl2.'>';
+                    $select3 .= $lvl2;
+                    $select3 .='</option>';
+
+                    if(is_array($array[$key][$lvl1][$lvl2])){
+                        foreach ($array[$key][$lvl1][$lvl2] as $lvl3 => $lvl3_val) {
+                            // echo '<pre>'; var_dump($array[$key]); echo '</pre>';
+                            $select4 .= '<option value='.$lvl3.'>';
+                            $select4 .= $lvl3;
+                            $select4 .='</option>';
+                            
+                        }
+                    }
+                }
+            }
+             
+
+        }
+
+    
     }
-
-   $select .= "</select>";
-
-   return $select;
+    
+    // print_r($array["B"][10]);
+    $select .= "</select>";
+    $select2 .= "</select>";
+    $select3 .= "</select>";
+    $select4 .= "</select>";
+    echo $select;
+    echo $select2;
+    echo $select3;
+    echo $select4;
+    
+//    return $select;
 }
 
 ?>
